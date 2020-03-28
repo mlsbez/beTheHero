4e247497518e80d88c3faa2bdab6cc2658d890ef
mlsbez <marianalimasb@gmail.com>
2020-03-28 17:57:47 -0300

    Creating Connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,6 +13,30 @@ app.use(routes);
 //     console.log(params);
 // })
 
+// app.get('/users', (request, response) => { /* criando a primeira rota. '/' = (localhost:3333). Uma função como segundo parâmetro */
+//     /* request: o que chega da requisição http, response o que é devolvido para o usuário */
+
+//     const params = request.query;
+
+//     console.log(params);
+    
+//     return response.json({
+//         evento: "Semana OmniStack 11.0",
+//         aluno: "Mariana Lima"
+//     });
+// });
+
+// app.post('/users', (request, response) => {
+//     const body = request.body;
+
+//     console.log(body);
+
+//     return response.json({
+//         evento: "Semana OmniStack 11.0",
+//         aluno: "Celiana Maria"
+//     });
+// })
+
 
 
 app.listen(3333); /* a porta da aplicação */
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,30 +1,27 @@
 const express = require('express');
+const crypto = require('crypto'); /* gerar uma string aleatória */
+
+const connection = require('./database/connection'); /* importar o connection.js */
+
 
 const routes = express.Router(); /* desacoplando o módulo de rotas em uma nova variável */
 
-routes.get('/users', (request, response) => { /* criando a primeira rota. '/' = (localhost:3333). Uma função como segundo parâmetro */
-    /* request: o que chega da requisição http, response o que é devolvido para o usuário */
-
-    const params = request.query;
-
-    console.log(params);
+routes.post('/ongs', async (request, response) => { 
+    const { name, email, whatsapp, city, uf } = request.body; /* acessar do corpo da requisição */
     
-    return response.json({
-        evento: "Semana OmniStack 11.0",
-        aluno: "Mariana Lima"
-    });
+    const id = crypto.randomBytes(4).toString('HEX'); /* gerar 4 bytes de caracteres aleatórios e converte numa string hexadecimal  */
+
+    await connection('ongs').insert({ /* o async junto com o await faz com que essa etapa seja concluída completamente para fluir o restante do código */
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf,
+    })
+
+    return response.json({ id }); /* é esse id que a ONG vai utilizar pra se conectar com a app */
 });
-
-routes.post('/users', (request, response) => {
-    const body = request.body;
-
-    console.log(body);
-
-    return response.json({
-        evento: "Semana OmniStack 11.0",
-        aluno: "Celiana Maria"
-    });
-})
 
 /* exportar uma variável para dentro de um arquivo */
 module.exports = routes; /* exportando as rotas para o index.js */
