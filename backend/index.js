9067aeffe704f7fe692f0fbc1950d2cdd6620f25
mlsbez <marianalimasb@gmail.com>
2020-03-28 15:47:24 -0300

    Installing Nodemon

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,36 @@ const express = require('express'); /* express contém todas as funcionalidades 
 
 const app = express(); /* variável que armazena a aplicação */
 
-app.get('/', (request, response) => { /* criando a primeira rota. '/' = (localhost:3333). Uma função como segundo parâmetro */
+app.use(express.json()); /* transformar json em js */
+
+app.get('/users', (request, response) => { /* criando a primeira rota. '/' = (localhost:3333). Uma função como segundo parâmetro */
+    /* request: o que chega da requisição http, response o que é devolvido para o usuário */
+
+    const params = request.query;
+
+    console.log(params);
+    
     return response.json({
         evento: "Semana OmniStack 11.0",
         aluno: "Mariana Lima"
     });
 });
+
+// app.get('/users/:id', (request, response) => {
+//     const params = request.params;
+
+//     console.log(params);
+// })
+
+app.post('/users', (request, response) => {
+    const body = request.body;
+
+    console.log(body);
+
+    return response.json({
+        evento: "Semana OmniStack 11.0",
+        aluno: "Celiana Maria"
+    });
+})
 
 app.listen(3333); /* a porta da aplicação */
