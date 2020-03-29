a661f08b3cba05df31578936968711186a472d03
mlsbez <marianalimasb@gmail.com>
2020-03-29 12:34:36 -0300

    Creating LogIn Page html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,13 @@
-import React, { useState } from 'react'; // useState para registrar o estado da app, ser mais dinâmico
+import React from 'react'; 
 
-import Header from './Header';
+import './global.css';
+
+import Logon from './pages/Logon';
+
 
 function App() {
-  const [counter, setCounter] = useState(0); // setCounter altera o valor do counter
-
-  // useState: retorna um Array [valor, funçãoDeAtualização]
-  function increment() {
-    setCounter(counter + 1);
-  }
-
   return (
-    // <Header title="Semana OmniStack" /> 
-    <div>
-      <Header>Contador: {counter}</Header> {/* { } quando for usar uma variável dentro do html */}
-      <button onClick={increment}>Incrementar</button> {/* vai chamar a função increment toda vez que clicar no botão */}
-    </div>
+    <Logon />
   );
 }
 
